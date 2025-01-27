16038a6b3ffaee66373ca6c178d8faadaa47543d
KaiYuanee <kaiyuanchang0620@gmail.com>
2025-01-27 12:23:27 +0800

    Create a009.cpp

---
+++ a009.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string str;
+    getline(cin, str);
+    for (int i = 0; i < str.size(); i++){
+        cout << char(str[i] - 7);
+    }
+    return 0;
+}
