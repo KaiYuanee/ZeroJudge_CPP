3f8ae9171362a57d34c03b112534db3e4dd9b6b4
KaiYuanee <kaiyuanchang0620@gmail.com>
2025-01-27 12:26:20 +0800

    Update a005.cpp

--- a005.cpp
+++ a005.cpp
@@ -7,8 +7,11 @@ int main(){
     cin >> t;
     for (int i = 0; i < t; i++){
         cin >> int1 >> int2 >> int3 >> int4;
-        if (int1 + int3 == int2 * 2) cout << int1 << ' ' << int2 << ' ' << int3 << ' ' << int4 << ' ' << int4 * 2 - int3 << endl;
-        else cout << int1 << ' ' << int2 << ' ' << int3 << ' ' << int4 << ' ' << int4 * int4 / int3 << endl;
+        if (int1 + int3 == int2 * 2){
+            cout << int1 << ' ' << int2 << ' ' << int3 << ' ' << int4 << ' ' << int4 * 2 - int3 << endl;
+        } else{
+            cout << int1 << ' ' << int2 << ' ' << int3 << ' ' << int4 << ' ' << int4 * int4 / int3 << endl;
+        }
     }
     return 0;
 }
