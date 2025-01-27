001dc073cfcb0ba23d31b62d095eb314c1b26a52
KaiYuanee <kaiyuanchang0620@gmail.com>
2025-01-27 12:37:39 +0800

    Update a004.cpp

--- a004.cpp
+++ a004.cpp
@@ -2,12 +2,17 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     int year;
-    while (cin >> year){
-        if ((year % 400 == 0) || ((year % 100 != 0) && (year % 4 == 0))){
+    while (cin >> year)
+    {
+        if ((year % 400 == 0) || ((year % 100 != 0) && (year % 4 == 0)))
+        {
             cout << "閏年\n";
-        } else{
+        }
+        else
+        {
             cout << "平年\n";
         }
     }
