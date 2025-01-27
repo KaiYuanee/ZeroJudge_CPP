73a4d6c69094f2bbcf611c0155ee7ea8b1201193
KaiYuanee <kaiyuanchang0620@gmail.com>
2025-01-27 12:39:00 +0800

    Update a006.cpp

--- a006.cpp
+++ a006.cpp
@@ -3,18 +3,24 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     int a, b, c, Dis, x1, x2;
     cin >> a >> b >> c;
     Dis = b * b - 4 * a * c;
-    if (Dis > 0){
+    if (Dis > 0)
+    {
         x1 = (b * -1 + int(sqrt(Dis))) / (a * 2);
         x2 = (b * -1 - int(sqrt(Dis))) / (a * 2);
         cout << "Two different roots x1=" << x1 << " , x2=" << x2;
-    } else if (Dis == 0){
+    }
+    else if (Dis == 0)
+    {
         x1 = (b * -1) / (a * 2);
         cout << "Two same roots x=" << x1;
-    } else{
+    }
+    else
+    {
         cout << "No real root";
     }
     return 0;
