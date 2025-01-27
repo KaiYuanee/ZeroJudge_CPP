9412e68593ee9c87bbe6ab0c9a0c7f63fd55f688
KaiYuanee <kaiyuanchang0620@gmail.com>
2025-01-27 12:40:11 +0800

    Update a010.cpp

--- a010.cpp
+++ a010.cpp
@@ -3,36 +3,51 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     int num, end_int, pow_count;
     bool first = true;
     cin >> num;
     end_int = num;
-    for (int i = 2; i <= sqrt(end_int); i++){
+    for (int i = 2; i <= sqrt(end_int); i++)
+    {
         pow_count = 0;
-        while (num % i == 0){
+        while (num % i == 0)
+        {
             num /= i;
             pow_count++;
         }
-        if (pow_count == 0) continue;
-        if (first){
-            if (pow_count == 1){
+        if (pow_count == 0)
+            continue;
+        if (first)
+        {
+            if (pow_count == 1)
+            {
                 cout << i;
                 first = false;
             }
-            if (pow_count > 1){
+            if (pow_count > 1)
+            {
                 cout << i << "^" << pow_count;
                 first = false;
             }
-        } else{
-            if (pow_count == 1) cout << " * " << i;
-            if (pow_count > 1) cout << " * " << i << "^" << pow_count;
         }
-        if (num == 1) break;
+        else
+        {
+            if (pow_count == 1)
+                cout << " * " << i;
+            if (pow_count > 1)
+                cout << " * " << i << "^" << pow_count;
+        }
+        if (num == 1)
+            break;
     }
-    if (num > 1){
-        if (first) cout << num;
-        else cout << " * " << num;
+    if (num > 1)
+    {
+        if (first)
+            cout << num;
+        else
+            cout << " * " << num;
     }
     return 0;
 }
