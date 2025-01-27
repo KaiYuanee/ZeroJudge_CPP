bd6255687007d0004197f2222ea21a143c62eac3
KaiYuanee <kaiyuanchang0620@gmail.com>
2025-01-27 12:33:23 +0800

    Update a015.cpp

--- a015.cpp
+++ a015.cpp
@@ -3,20 +3,20 @@
 using namespace std;
 
 int main(){
-	int row, column;
-	while (cin >> row >> column){
-	    int matrix[row + 1][column + 1];
-	    for (int i = 0; i < row; i++){
-		    for (int j = 0; j < column; j++){
-			    cin >> matrix[i][j];
-		    }
-	    }
-	    for (int i = 0; i < column; i++){
-		    for (int j = 0; j < row; j++){
-			    cout << matrix[j][i] << ' ';
-		    }
-		    cout << endl;
-	    }
-	}
-	return 0;
+    int row, column;
+    while (cin >> row >> column){
+        int matrix[row + 1][column + 1];
+        for (int i = 0; i < row; i++){
+            for (int j = 0; j < column; j++){
+                cin >> matrix[i][j];
+            }
+        }
+        for (int i = 0; i < column; i++){
+            for (int j = 0; j < row; j++){
+                cout << matrix[j][i] << ' ';
+            }
+            cout << endl;
+        }
+    }
+    return 0;
 }
