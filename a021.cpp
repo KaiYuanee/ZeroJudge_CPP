3b3a68b7708286943930a1607f6ea69c1b3a9a4b
KaiYuanee <kaiyuanchang0620@gmail.com>
2025-02-13 21:06:10 +0800

    Update a021.cpp

--- a021.cpp
+++ a021.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
+
+int compare(string num1, string num2)
+{
+    if (num1.length() < num2.length())
+    {
+        return -1;
+    }
+    if (num1.length() > num2.length())
+    {
+        return 1;
+    }
+    if (num1 < num2)
+    {
+        return -1;
+    }
+    if (num1 > num2)
+    {
+        return 1;
+    }
+    return 0;
+}
 
 string add(string num1, string num2)
 {
@@ -15,7 +37,7 @@ string add(string num1, string num2)
         num2 = "0" + num2;
     }
     string value = "";
-    int carry, sum;
+    int carry = 0, sum;
     for (int i = num1.length() - 1; i >= 0; i--)
     {
         sum = (num1[i] - '0') + (num2[i] - '0') + carry;
@@ -33,7 +55,7 @@ string add(string num1, string num2)
 string sub(string num1, string num2)
 {
     bool is_negative = false;
-    if (num1 < num2)
+    if (compare(num1, num2) == -1)
     {
         swap(num1, num2);
         is_negative = true;
@@ -43,7 +65,7 @@ string sub(string num1, string num2)
         num2 = "0" + num2;
     }
     string value = "";
-    int borrow, diff;
+    int borrow = 0, diff;
     for (int i = num1.length() - 1; i >= 0; i--)
     {
         diff = (num1[i] - '0') - (num2[i] - '0') - borrow;
@@ -72,16 +94,59 @@ string sub(string num1, string num2)
 
 string mul(string num1, string num2)
 {
-    int len1 = num1.length(), len2 = num2.length(), product, sum;
+    reverse(num1.begin(), num1.end());
+    reverse(num2.begin(), num2.end());
+    int len1 = num1.length();
+    int len2 = num2.length();
     vector<int> value(len1 + len2, 0);
-    for (int i = len1 - 1; i >= 0; i--)
+    for (int i = 0; i < len1; i++)
     {
-        for (int j = len2 - 1; i >= 0; i--)
+        for (int j = 0; j < len2; j++)
         {
-            product = (num1[i] - '0') * (num2[i] - '0');
-            sum += 
+            value[i + j] += (num1[i] - '0') * (num2[j] - '0');
         }
     }
+    for (int i = 0; i < value.size() - 1; i++)
+    {
+        value[i + 1] += value[i] / 10;
+        value[i] %= 10;
+    }
+    while (value.size() > 1 && value.back() == 0)
+    {
+        value.pop_back();
+    }
+    string product;
+    for (int i : value)
+    {
+        product += i + '0';
+    }
+    reverse(product.begin(), product.end());
+    return product;
+}
+
+string div(string num1, string num2)
+{
+    if (compare(num1, num2) == -1)
+    {
+        return "0";
+    }
+    string quotient = "", remainder = "";
+    for (char digit : num1)
+    {
+        remainder += digit;
+        int count = 0;
+        while (compare(remainder, num2) != -1)
+        {
+            remainder = sub(remainder, num2);
+            count++;
+        }
+        quotient += count + '0';
+    }
+    while (quotient.length() > 1 && quotient[0] == '0')
+    {
+        quotient.erase(quotient.begin());
+    }
+    return quotient;
 }
 
 int main()
