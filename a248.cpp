f80c7bc8e80f7e35473bccdd2f10434234e4ad28
KaiYuanee <kaiyuanchang0620@gmail.com>
2025-02-13 16:18:56 +0800

    Update a248.cpp

--- a248.cpp
+++ a248.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+int main()
+{
+    int a, b, N;
+    while (cin >> a >> b >> N)
+    {
+        vector<int> after_point;
+        int rest = a % b;
+        for (int i = 0; i < N; i++)
+        {
+            rest *= 10;
+            after_point.push_back(rest / b);
+            rest %= b;
+        }
+        cout << a / b << '.';
+        for (int i : after_point)
+        {
+            cout << i;
+        }
+        cout << endl;
+    }
+    return 0;
+}
