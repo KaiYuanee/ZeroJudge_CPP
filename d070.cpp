4585110ef1c30990e044499f8ffc6f82e3d7ffc4
KaiYuanee <kaiyuanchang0620@gmail.com>
2025-03-16 20:45:28 +0800

    Create d070.cpp

---
+++ d070.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int y;
+    while (1)
+    {
+        cin >> y;
+        if (y == 0)
+        {
+            break;
+        }
+        if ((!(y % 4) && (y % 100)) || !(y % 400))
+        {
+            cout << "a leap year" << endl;
+        }
+        else
+        {
+            cout << "a normal year" << endl;
+        }
+    }
+    return 0;
+}
