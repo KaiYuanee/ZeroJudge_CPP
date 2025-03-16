7a9e70753203c73bb4d7443cb274318bf9b36aef
KaiYuanee <kaiyuanchang0620@gmail.com>
2025-03-16 16:04:15 +0800

    Create d064.cpp

---
+++ d064.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    cin >> i;
+    if (i % 2)
+    {
+        cout << "Odd" << endl;
+    }
+    else
+    {
+        cout << "Even" << endl;
+    }
+    return 0;
+}
