4acaa0f1fdec90abd1c91c44f816499d5dd2237d
KaiYuanee <kaiyuanchang0620@gmail.com>
2025-03-17 16:08:35 +0800

    Create d392.cpp

---
+++ d392.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    string line;
+    while (getline(cin, line))
+    {
+        stringstream ss(line);
+        int num, sum = 0;
+        while (ss >> num)
+        {
+            sum += num;
+        }
+        cout << sum << endl;
+    }
+    return 0;
+}
