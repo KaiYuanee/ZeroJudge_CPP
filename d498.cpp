02ce50d444c10ca00ea3968eddddf605a12523bf
KaiYuanee <kaiyuanchang0620@gmail.com>
2025-04-08 13:06:05 +0800

    Create d498.cpp

---
+++ d498.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int t = 0; t < n; t++)
+    {
+        cout << "I don't say swear words!" << endl;
+    }
+    return 0;
+}
